88112dc88ac367621521b4ff6fe2dee092709be0
Ryan Rampersad <ryan.rampersad@gmail.com>
2014-01-25 17:06:49 -0600

    Adding client side human timme diff updates.
    JS now reads the time>datetime attrib and will set the span.ago accordingly

--- library/js/script.js
+++ library/js/script.js
@@ -77,12 +77,71 @@
 
 	}
 
+	function human_time_difference(from, to) {
+		to = to || Date.now();
+
+		// convert MS to S
+		to = to / 1000;
+		from = from / 1000;
+
+		var periods = {
+			'minutes' : ['%s minute', '%s minutes'],
+			'hours' : ['%s hour', '%s hours'],
+			'days' : ['%s day', '%s days'],
+			'weeks' : ['%s week', '%s weeks'],
+			'months' : ['%s month', '%s months'],
+			'years' : ['%s year', '%s years']
+		};
+
+		var ranges = {
+			'years' : 31556926,
+			'months' : 2592000,
+			'weeks' : 604800,
+			'days' : 86400,
+			'hours' : 3600,
+			'minutes' : 60
+		};
+
+		var diff = Math.abs(to - from);
+		var since = '';
+
+		$.each(ranges, function(key, value){
+			if ( diff <= value ) return; // jquery continue
+
+			var time = Math.round(diff / value);
+			if (time < 1) {
+				time = 1;
+			}
+
+			since = (time == 1 ? periods[key][0] : periods[key][0]).replace('%s', time);
+			return false; // jquery break
+
+		});
+
+
+
+		return since;
+
+	}
+
+	function setup_human_date_time() {
+
+		$('time span.ago').each(function(index, element){
+			var time = $(element).parent('time');
+			var raw = time.attr('datetime');
+			var diff = human_time_difference( Date.parse(raw) );
+			$(element).html(diff + " ago");
+		});
+
+	}
+
 	$(document).ready(function(){
 
 		setup_mejs_rails_fix();
 		setup_idle_nexus();
 		setup_reveal_share();
 		setup_shownote_clicks();
+		setup_human_date_time();
 
 	});
 
