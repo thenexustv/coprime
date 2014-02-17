e26849ff69aa88513f64d78c42f032e2e44be2a3
Ryan Rampersad <ryan.rampersad@gmail.com>
2014-02-17 02:55:27 -0600

    Adding the primary configuration files
    Updated coprime to handle new locations of files

---
+++ config.rb
@@ -0,0 +1,34 @@
+# Compass is a great cross-platform tool for compiling SASS. 
+# This compass config file will allow you to 
+# quickly dive right in.
+# For more info about compass + SASS: http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
+
+require "zurb-foundation"
+
+
+#########
+# 1. Set this to the root of your project when deployed:
+project_type = :stand_alone
+
+http_path = "/"
+# 2. probably don't need to touch these
+css_dir = "resources/css/build"
+sass_dir = "resources/css/source"
+images_dir = "resources/images"
+javascripts_dir = "resources/js"
+fonts_dir = "resources/fonts"
+environment = :development
+relative_assets = true
+
+
+# 3. You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+# 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
+# output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# don't touch this
+preferred_syntax = :scss
