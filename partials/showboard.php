72bf41fa5dee289d3e1e336695fc05c9491704fa
Ryan Rampersad <ryan.rampersad@gmail.com>
2014-03-19 13:56:46 -0500

    Finished modern boxes responsive system

--- partials/showboard.php
+++ partials/showboard.php
@@ -13,14 +13,10 @@ $cp_series = new Coprime_Series($series);
 	
 	<div class="candy">
 		
-		<!-- <div class="overlay"></div> -->
-
 		<h3 class="show-title"><?php echo $cp_episode->get_title(); ?></h3>
 		<h4 class="show-number"><?php echo $cp_series->get_series_name(); ?> #<?php echo $cp_episode->get_episode_number(); ?></h4>
 		<h4 class="show-date"><?php echo $cp_episode->get_posted_date(); ?> <?php echo $cp_episode->get_is_new(); ?></h4>
-		<!-- <div class="show-description"><?php echo $cp_episode->get_episode_description(); ?></div> -->
-		<!-- SEE: http://www.tomshardware.com/answers/id-1847117/amd-8350-music-production.html -->
-		<!-- and https://www.ableton.com/ -->
+
 	</div>
 
 	</div>
